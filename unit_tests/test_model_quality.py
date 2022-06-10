33f73f498089dd8dcec26cb572985e0a903e34ad
wmikolajczyk <mikolajczyk.wojtas@gmail.com>
2022-06-10 12:52:35 +0200

    Fix types

--- unit_tests/test_model_quality.py
+++ unit_tests/test_model_quality.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from utils import evaluate
@@ -5,7 +6,9 @@ from utils import evaluate
 
 def test_model_quality(model_for_test):
     X = pd.DataFrame({"number": [0, 1, 2, 3, 4, 5, 10, 15, 23, 30]})
-    y = ["FizzBuzz", "", "", "Fizz", "", "Buzz", "Buzz", "FizzBuzz", "", "FizzBuzz"]
+    y = np.array(
+        ["FizzBuzz", "", "", "Fizz", "", "Buzz", "Buzz", "FizzBuzz", "", "FizzBuzz"]
+    )
     preds = model_for_test.predict(X)
     metrics = evaluate(y, preds)
     assert metrics["f1"] >= 0.8
